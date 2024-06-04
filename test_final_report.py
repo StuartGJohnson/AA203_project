a085a46396594fc46a32a3f9ce17895ad579af49
sjohnson <stuart.g.johnson@gmail.com>
2024-06-04 10:23:27 -0700

    Add ampersands for the latex table.

--- test_final_report.py
+++ test_final_report.py
@@ -74,8 +74,9 @@ def collect_data(rdir, annot):
     wsum = np.sum(w, axis=1)
     ucumsum = env.dt * np.cumsum(np.sum(u, axis=1))
     print(annot)
-    print(f"raw: {csum[-1]:.2f}, {psum[-1]:.2f}, {wsum[-1]:.2f}, {ucumsum[-1]:.2f}")
-    print(f"norm: {csum[-1]/csum[-1]:.2f}, {psum[-1]/csum[-1]:.2f}, {wsum[-1]/csum[-1]:.2f}, {ucumsum[-1]/csum[-1]:.2f}")
+    # the ampersands are for a latex table!
+    print(f"raw: {csum[-1]:.2f} & {psum[-1]:.2f} & {wsum[-1]:.2f} & {ucumsum[-1]:.2f}")
+    print(f"norm: {csum[-1]/csum[-1]:.2f} & {psum[-1]/csum[-1]:.2f} & {wsum[-1]/csum[-1]:.2f} & {ucumsum[-1]/csum[-1]:.2f}")
 
 class MyTestCase2(unittest.TestCase):
     def test_fr_sim_no_control(self):
